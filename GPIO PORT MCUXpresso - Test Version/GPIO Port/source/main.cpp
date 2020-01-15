3acf805efce65a7ed54dffb7d4b5971cd96ddb3b
Anilton WinHomePC <aclr@icomp.ufam.edu.br>
2020-01-15 04:57:25 -0400

    init GPIO Port Interrupt

--- GPIO PORT MCUXpresso - Test Version/GPIO Port/source/main.cpp
+++ GPIO PORT MCUXpresso - Test Version/GPIO Port/source/main.cpp
@@ -6,8 +6,7 @@ mkl_GPIOPort greenLed(gpio_PTB19);
 
 mkl_GPIOPort pushButton(gpio_PTA2);
 
-void setup()
-{
+void setup() {
 
 	blueLed.setPortMode(gpio_output);
 	redLed.setPortMode(gpio_output);
@@ -17,49 +16,38 @@ void setup()
 	pushButton.setPullResistor(gpio_pullUpResistor);
 }
 
-int main()
-{
+int main() {
 	uint32_t contador = 0;
 	uint8_t select = 0;
 	setup();
 
-	while (true)
-	{
-		if (pushButton.readBit())
-		{
+	while (true) {
+		if (pushButton.readBit()) {
 			contador++;
-			if (contador >= 50000)
-			{
+			if (contador >= 50000) {
 				select++;
-				if (select == 3)
-				{
+				if (select == 3) {
 					select = 0;
 				}
 
 				contador = 0;
 			}
 
-			if (select == 0)
-			{
+			if (select == 0) {
 				redLed.writeBit(0);
 				blueLed.writeBit(1);
 				greenLed.writeBit(1);
-			}
-			else if (select == 1)
-			{
+			} else if (select == 1) {
+				redLed.writeBit(1);
+				blueLed.writeBit(1);
+				greenLed.writeBit(0);
+
+			} else if (select == 2) {
 				redLed.writeBit(1);
 				blueLed.writeBit(0);
 				greenLed.writeBit(1);
 			}
-			else if (select == 2)
-			{
-				redLed.writeBit(1);
-				blueLed.writeBit(1);
-				greenLed.writeBit(0);
-			}
-		}
-		else
-		{
+		} else {
 			redLed.writeBit(1);
 			blueLed.writeBit(1);
 			greenLed.writeBit(1);
